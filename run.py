f89d66ae4a5c3c03c9b56f6f1ae86fc3c3d5a392
Freddy Fransson <freddyfransson@hotmail.com>
2023-06-20 13:33:49 +0000

    add zip to choise

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 # Shopping cart exercise
 
-sushi = ["1.  Dragon Roll", "2.  Rainbow Roll", "3.  Salmon Roll", "4.  Crispy Roll", "5.  Tempura Roll"]
-miso = 0.5
+MISO = 0.5
 prices = [1.7, 1.2, 1, 1.5, 1.6]
 choises = []
 choise_costs = []
 total = 0
-
-print(sushi)
 
 def main():
     startup_menue()
     
 
 def startup_menue():
+
+    sushi = ["1.  Dragon Roll", "2.  Rainbow Roll", "3.  Salmon Roll", "4.  Crispy Roll", "5.  Tempura Roll"]
+
     print(
         """
         *************************************************
@@ -23,19 +23,19 @@ def startup_menue():
         *************************************************
         """
     )
+# Credit to scb at stack overflow
+    print("\n".join("{} {}".format(x, y) for x, y in zip(sushi, prices)))
 
+    while True:
+        choise = input("Please enter the number of the roll that you want to order, or press 'q' if you have changed your mind: ")
+        if choise.lower() == "q":
+            break
+        else:
+            #price = float(input(f"Enter the price of a {food}: $"))
+            choises.append(choise)
+            prices.append(price)
 
-
-
-
-while True:
-    sushi = input("Please enter the number of the roll that you want to order, or press 'q' if you have changed your mind: ")
-    if sushi.lower() == "q":
-        break
-    else:
-        price = float(input(f"Enter the price of a {food}: $"))
-        foods.append(food)
-        prices.append(price)
+main()
 
 print("----- YOUR CART -----")
 
@@ -48,4 +48,3 @@ for price in prices:
 print()
 print(f"Your total is: ${total}")
 
-main()
