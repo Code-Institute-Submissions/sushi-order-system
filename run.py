0c0afed050625fbf33dc12dbffe5a3189f9aec6f
Freddy Fransson <freddyfransson@hotmail.com>
2023-06-21 10:35:04 +0000

    add euros and final order system

--- run.py
+++ run.py
@@ -1,17 +1,18 @@
-# Shopping cart exercise
+# Global variables for summarizing order choises
 
-sushi = ["1.  Dragon Roll", "2.  Rainbow Roll", "3.  Salmon Roll", "4.  Crispy Roll", "5.  Tempura Roll"]
+sushi = ["1.Dragon Roll", "2.Rainbow Roll", "3.Salmon Roll", "4.Crispy Roll", "5.Tempura Roll"]
+euros = ["€", "€", "€", "€", "€"]
 prices = [1.7, 1.2, 1, 1.5, 1.6]
 choises = []
 choise_costs = []
 chosen_amount = []
 total = 0
-
+# Main program
 def main():
     welcome()
     startup_menu()
     
-
+# Welcome text
 def welcome():
 
     print(
@@ -19,27 +20,29 @@ def welcome():
         *************************************************
         * Welcome to sushi Paradise Ordering System!    *
         *                                               *
-        * Please order from the menu below             *
+        * Please order from the menu below              *
         *************************************************
+
         """
     )
     
-
+# Menu function
 def startup_menu():
 
     # Credit to scb at stack overflow
-    print("\n".join("{} {}".format(x, y) for x, y in zip(sushi, prices)))
+    print("\n".join("{} {} {}".format(x, y, z) for x, y, z in zip(sushi, prices, euros)))
 
     pick_item(sushi)
 
 def pick_item(sushi):
 
     choise = int(input("Please enter the number of the roll that you want to order, or press '0' if you have changed your mind: "))
-
+    # Amount and cost function for sushi ordering system
     while choise != 0:
         if choise == 1 or choise == 2 or choise == 3 or choise == 4 or choise == 5:
             print(f"\nYou have selected {sushi[choise -1]} which costs {prices[choise -1]} euros.\n")
             amount = int(input("How many pieces would you like? \n"))
+            # Datavalidation for correct input data
             if amount > 0 and amount < 50:
                 price = prices[choise -1]
                 choise_costs.append(str(round(price * amount, 2)) + " €")
@@ -57,30 +60,24 @@ def pick_item(sushi):
                     print("\nYou have entered invalid data, please try again.\n")
                     pick_item(sushi)
                     break
+            # Datavalidation for correct input data           
             else:
                 print("\nYou have entered invalid data, please try again.\n")
                 pick_item(sushi)
                 break
-            
-            
             break
+        # Datavalidation for correct input data 1-5             
         else:
             print("\nYou have entered a number that does not exist on our menu, please choose number 1-5 \n")
             pick_item(sushi)
             break
 
-    # while True:
-    #     choise = input("Please enter the number of the roll that you want to order, or press 'q' if you have changed your mind: ")
-    #     if choise.lower() == "q":
-    #         break
-    #     else:
-    #         price = float(input(f"Enter the price of a {food}: $"))
-    #         choises.append(choise)
-    #         prices.append(price)
-def order_summary():
 
+def order_summary():
+# Total amount and cost from the order
     print("\n".join("{} {} {}".format(x, y, z) for x, y, z in zip(choises, chosen_amount, choise_costs)))
-    print(str(round(total, 2)))
+    print(str(round(total, 2)) + " €")
+#Function for miso order 
     order_miso = str(input("Would you also want some miso for a cost of 0.5€ per serving? If you do, please press y. If not, press n. \n"))
     if order_miso.lower() == "y":
         pick_miso()
@@ -90,7 +87,7 @@ def order_summary():
         print("\nYou have entered invalid data, please try again.\n")
         order_summary()
 
-
+# Miso order amount function
 def pick_miso():
     miso_amount = int(input("How many servings would you like? \n"))
     if miso_amount > 0 and miso_amount < 50:
@@ -104,22 +101,31 @@ def pick_miso():
         print("\nYou have entered invalid data, please try again.\n")
         pick_miso()
         
-
+# Final order cost and choises function
 def finalize_order():
-    print("Finalize")
+    global choises
+    global choise_costs
+    global chosen_amount
+    global total
+    print("This is your final order summary: ")
     print("\n".join("{} {} {}".format(x, y, z) for x, y, z in zip(choises, chosen_amount, choise_costs)))
-    print(str(round(total, 2)))
+    print(f"Your total cost for this order is {str(round(total, 2))} €")
+    checkout = str(input("Would you like to continue and finalize your order? To proceed to checkout press y, to cancel order press n \n"))
+    if checkout.lower() == "y":
+        final_order()
+    elif checkout.lower() == "n":
+        choises = []
+        choise_costs = []
+        chosen_amount = []
+        total = 0
+        main()
+    else:
+        print("\nYou have entered invalid data, please try again.\n")
+        finalize_order()
+
+
+def final_order():
+    print("Thank you so much for ordering from Sushi Paradise! Come again! Hai! Kampai!")
+
 
 main()
-
-# print("----- YOUR CART -----")
-
-# for food in foods:
-#     print(food, end=" ")
-
-# for price in prices:
-#     total += price
-
-# print()
-# print(f"Your total is: ${total}")
-
